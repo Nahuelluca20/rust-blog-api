4d0d5ddd9608c7adc14ec756c96517c3135cd25f
Nahuel Luca <nahuelluca20@gmail.com>
2023-07-17 22:04:50 -0300

    connect pool db

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate diesel;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 pub mod models;
@@ -10,9 +11,11 @@ use std::env;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
 
 #[get("/")]
-async fn hello_word() -> impl Responder {
+async fn hello_world() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -20,11 +23,19 @@ async fn hello_word() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let db_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut connection = PgConnection::establish(&db_url).expect("Error connecting to db");
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
 
-    HttpServer::new(|| App::new().service(hello_word))
-        .bind(("127.0.0.1", 8080))
-        .unwrap()
-        .run()
-        .await
+    let pool = Pool::builder()
+        .max_size(5)
+        .build(connection)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
+        App::new()
+        .service(hello_world)
+        .app_data(web::Data::new(pool.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
